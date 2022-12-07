f110636a5c4b79fb91389f176c4caea811250569
Alex Luong <alex0707101@gmail.com>
2022-12-07 02:01:10 -0800

    Made main menu

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ const canText = document.getElementById('text-canvas')
 const ctxText = canText.getContext("2d")
 const background = document.getElementById("background")
 const ctxBG = background.getContext("2d")
-const startGameButton = document.getElementById("start-game")
+const startGameButton = document.getElementById("start-button")
+
 
 startGameButton.addEventListener('click', startGame)
 
@@ -33,10 +34,10 @@ function startGame(){
   document.getElementById('vol-container').onclick = () =>
   {
     if (game.theme.volume === 0) {
-          game.theme.volume = 0.0; //set back to 0.7 later 
-          game.turn.volume = 0.0;
-          game.win.volume = 0.0;
-          game.wind.volume = 0.0;
+          game.theme.volume = 0.7; //set back to 0.7 later 
+          game.turn.volume = 0.7;
+          game.win.volume = 0.7;
+          game.wind.volume = 0.7;
           document.getElementById('high-volume').classList.remove('hidden');
           document.getElementById('mute').classList.add('hidden');
     } else {
